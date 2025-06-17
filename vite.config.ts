8302adba44cdce2c29ca55a24d4429eb15bb6a53
skandvijay <skand.vijay@capacity.com>
2025-06-17 11:42:14 -0500

    Update V1

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Source-Filter-Mockup/', // 👈 THIS is needed for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
